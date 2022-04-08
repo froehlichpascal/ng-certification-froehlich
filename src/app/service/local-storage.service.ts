8722bf1af98fea73a4510e6d1dce42139fea84d7
froehlichpascal <mail@pascalfroehlich.at>
2022-04-08 10:50:19 +0200

    1 file modified

--- src/app/service/local-storage.service.ts
+++ src/app/service/local-storage.service.ts
@@ -1,49 +1,51 @@
-import {Injectable} from '@angular/core';
-import {element, error} from "protractor";
+import { Injectable } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalStorageService {
+  private static readonly STORAGE_KEY_ZIP_CODES = 'ZIP_CODES';
 
-    private static readonly STORAGE_KEY_ZIP_CODES = "ZIP_CODES";
+  constructor() {}
 
-    constructor() {}
+  saveZip(zip: number): number[] {
+    let zips = this.loadZips();
+    zips.unshift(zip);
+    LocalStorageService.saveZips(zips);
+    return zips;
+  }
 
-    saveZip(zip: number): number[] {
-        let zips = this.loadZips();
-        zips.unshift(zip);
-        LocalStorageService.saveZips(zips);
-        return zips;
+  loadZips(): number[] {
+    let zipsInJson = localStorage.getItem(
+      LocalStorageService.STORAGE_KEY_ZIP_CODES
+    );
+    if (zipsInJson) {
+      try {
+        return JSON.parse(zipsInJson);
+      } catch (e) {
+        console.error('Error parsing zips from ' + zipsInJson, e);
+      }
+    } else {
+      return [];
     }
+  }
 
-    loadZips(): number[] {
-        let zipsInJson = localStorage.getItem(LocalStorageService.STORAGE_KEY_ZIP_CODES);
-        if (zipsInJson) {
-            try {
-                return JSON.parse(zipsInJson);
-            } catch (e) {
-                console.error('Error parsing zips from ' + zipsInJson, e);
-            }
-        } else {
-            return [];
-        }
-
+  static saveZips(savedZips: number[]) {
+    try {
+      localStorage.setItem(
+        LocalStorageService.STORAGE_KEY_ZIP_CODES,
+        JSON.stringify(savedZips)
+      );
+    } catch (e) {
+      console.error('Error saving' + savedZips + 'to local Stroage', e);
     }
+  }
 
-    static saveZips(savedZips: number[]) {
-        try {
-            localStorage.setItem(LocalStorageService.STORAGE_KEY_ZIP_CODES, JSON.stringify(savedZips));
-        } catch (e) {
-            console.error('Error saving' + savedZips + 'to local Stroage', e);
-        }
-    }
+  deleteZipByIndex(index: number): number[] {
+    let zips = this.loadZips();
+    zips.splice(index, 1);
 
-    deleteZipByIndex(index: number): number[] {
-        let zips = this.loadZips();
-        zips.splice(index, 1);
-
-        LocalStorageService.saveZips(zips);
-        return zips;
-    }
+    LocalStorageService.saveZips(zips);
+    return zips;
+  }
 }
